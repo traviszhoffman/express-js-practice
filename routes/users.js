f59eb77cef91641f5aa83e170f6cdfd7ee99d835
Travis Hoffman <traviszh@byu.edu>
2023-04-06 10:25:11 -0600

    finished expressjs course

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,52 @@
 const express = require('express')
 const router = express.Router()
 
-app.get('/', (req, res) => {
+//pay attention to ordering of routes can't put :id thats dynamic put static above dynamic routes
+router.use(logger)
+router.get('/', (req, res) => {
+    req.query.name
     res.send("User List")
 })
-app.get('/new', (req,res) => {
-    res.send("User New Form")
+router.get('/new', (req,res) => {
+    res.render("users/new", {firstName: "Test"})
 })
 
-module.exports
+router.post('/', (req, res)=> {
+    const isValid = true
+    if (isValid) {
+        users.push({ firstName: req.body.firstName})
+        res.redirect(`/users/${users.length-1}`)
+    } else {
+        console.log("Error")
+        res.render('users/new', {firstName: req.body.firstName})
+    }
+})
+
+router
+  .route("/:id")
+  .get((req, res) => {
+    console.log(req.user)
+    res.send(`User Get with id ${req.params.id}`)
+  })
+  .put((req, res) => {
+    res.send(`User Update with id ${req.params.id}`)
+  })
+  .delete((req, res) => {
+    res.send(`User Delete with id ${req.params.id}`)
+  })
+
+const users = [{ name: "kyle"}, { name: 'Sally'}]
+//helps us to save users one version of middleware
+
+router.param("id", (req, res, next, id) => {
+    req.user = users[id]
+    next()
+})
+
+function logger(req, res, next) {
+    console.log(req.originalUrl)
+    next()
+}
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -2,15 +2,23 @@ const express = require('express')
 const app =express()
 
 app.set('view engine', 'ejs')
+app.use(express.static("public"))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+//app.use(logger)
 
-app.get('/', (req, res) => {
-    console.log("Here")
-    //res.json({ message: 
-    //"Error"})
-    //res.send('Hi')
-    res.render('index', { text: "world"})
-})
+//can apply middleware to only ceertain requests
+
+
 //res.download res.send res.status res.render
 
+const userRouter = require('./routes/users')
+
+app.use('/users', userRouter)
+
+function logger(req, res, next) {
+    console.log(req.originalUrl)
+    next()
+}
 
 app.listen(3000)
